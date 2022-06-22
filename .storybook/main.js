fcbd3a75499149da4a09e12a36f062fd5f261b89
Sushil Parajuli <sushilparajuli2010@gmail.com>
2022-06-22 22:48:16 +0400

    Project initialize with unocss, storybook setup

---
+++ .storybook/main.js
@@ -0,0 +1,26 @@
+const Inspect = require('vite-plugin-inspect');
+const Unocss = require('unocss/vite');
+
+module.exports = {
+  "stories": [
+    "../src/**/*.stories.mdx",
+    "../src/**/*.stories.@(js|jsx|ts|tsx)"
+  ],
+  "addons": [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions"
+  ],
+  "framework": "@storybook/vue3",
+  "core": {
+    "builder": "@storybook/builder-vite"
+  },
+  "features": {
+    "storyStoreV7": true
+  },
+  async viteFinal(config, { configType }) {
+    config.plugins.push(Unocss.default());
+    config.plugins.push(Inspect());
+    return config;
+  }
+}
---
+++ src/stories/Button.stories.ts
@@ -0,0 +1,53 @@
+import MyButton from "./Button.vue";
+import { Meta, StoryFn } from "@storybook/vue3";
+
+// More on default export: https://storybook.js.org/docs/vue/writing-stories/introduction#default-export
+export default {
+  title: "Example/Button",
+  component: MyButton,
+  // More on argTypes: https://storybook.js.org/docs/vue/api/argtypes
+  argTypes: {
+    backgroundColor: { control: "color" },
+    onClick: {},
+    size: {
+      control: { type: "select" },
+      options: ["small", "medium", "large"],
+    },
+  },
+} as Meta<typeof MyButton>;
+
+// More on component templates: https://storybook.js.org/docs/vue/writing-stories/introduction#using-args
+const Template: StoryFn<typeof MyButton> = (args) => ({
+  // Components used in your story `template` are defined in the `components` object
+  components: { MyButton },
+  // The story's `args` need to be mapped into the template through the `setup()` method
+  setup() {
+    return { args };
+  },
+  // And then the `args` are bound to your component with `v-bind="args"`
+  template: '<my-button v-bind="args" />',
+});
+
+export const Primary: StoryFn<typeof MyButton> = Template.bind({});
+// More on args: https://storybook.js.org/docs/vue/writing-stories/args
+Primary.args = {
+  primary: true,
+  label: "Button",
+};
+
+export const Secondary: StoryFn<typeof MyButton> = Template.bind({});
+Secondary.args = {
+  label: "Button",
+};
+
+export const Large: StoryFn<typeof MyButton> = Template.bind({});
+Large.args = {
+  size: "large",
+  label: "Button",
+};
+
+export const Small: StoryFn<typeof MyButton> = Template.bind({});
+Small.args = {
+  size: "small",
+  label: "Button",
+};
